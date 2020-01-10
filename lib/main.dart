db014e72c144bd73bbda71176d793c3cff30c2bd
Ahad Islam <ahad.islam624@gmail.com>
2020-01-10 12:18:57 -0500

    added a launch josn new customClass to check how animations work. building entire class to loop an rendered circle(hypnotic...)

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,106 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(CircleRoute());
+void main() => runApp(MyApp());
 
-class CircleRoute extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return MaterialApp(
+      title: 'Wave Animation',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CircleWaveRoute(),
+    );
   }
-
 }
 
-class CirclePainter extends CustomPainter {
+class CircleWaveRoute extends StatefulWidget {
   @override
+  _CircleWaveRouteState createState() => _CircleWaveRouteState();
+}
 
-  var wavePaint = Paint()
-  ..color = Colors.black
-  ..style = PaintingStyle.stroke
-  ..strokeWidth = 2.0
-  ..isAntiAlias = true;
+class _CircleWaveRouteState extends State<CircleWaveRoute>
+    with SingleTickerProviderStateMixin {
+  double waveRadius = 2.0;
+  double waveGap = 10.0;
+  Animation<double> _animation;
+  AnimationController controller;
 
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: Duration(milliseconds: 20000), vsync: this);
+
+    controller.forward();
+
+    controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reset();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _animation = Tween(begin: 0.0, end: waveGap).animate(controller)
+      ..addListener(() {
+        setState(() {
+          waveRadius = _animation.value;
+        });
+      });
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomPaint(
+        size: Size(double.infinity, double.infinity),
+        painter: CircleWavePainter(waveRadius),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class CircleWavePainter extends CustomPainter {
+  final double waveRadius;
+  var wavePaint;
+  CircleWavePainter(this.waveRadius) {
+    wavePaint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5.0
+      ..isAntiAlias = true;
+  }
+  @override
   void paint(Canvas canvas, Size size) {
     double centerX = size.width / 2.0;
     double centerY = size.height / 2.0;
-    canvas.drawCircle(Offset(centerX, centerY), 100.0, wavePaint);
+    double maxRadius = hypot(centerX, centerY);
+
+    var currentRadius = waveRadius;
+    while (currentRadius < maxRadius) {
+      canvas.drawCircle(Offset(centerX, centerY), currentRadius, wavePaint);
+      currentRadius += 10.0;
     }
+  }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return null;
+  bool shouldRepaint(CircleWavePainter oldDelegate) {
+    return oldDelegate.waveRadius != waveRadius;
   }
-  
+
+  double hypot(double x, double y) {
+    return math.sqrt(x * x + y * y);
+  }
 }
